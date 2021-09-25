2eb0327a7ff22700bea8bc6f49d85e5aab19d92b
beslimir <marin.beslo@gmail.com>
2021-09-25 20:50:20 +0200

    fixing crash - update retrofit version

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -1,10 +1,15 @@
 package com.example.newsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.RetrofitInstance.Companion.api
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +17,10 @@ class NewsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bottomNavigationMenu.setupWithNavController(navHostFragment.findNavController())
+
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.i("test", "${api.getLatestNews()}")
+        }
+
     }
 }
